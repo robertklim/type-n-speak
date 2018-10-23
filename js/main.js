fd2861ed96c4248ed68a4a2b9c3e6ada374c36dc
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-23 19:02:31 +0200

    Load voices into select tag

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    options = {}
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
+const synth = window.speechSynthesis;
+
+const textForem = document.querySelector('form')
+const textInput = document.querySelector('#text-input')
+const voiceSelect = document.querySelector('#voice-select')
+const rate = document.querySelector('#rate')
+const pitch = document.querySelector('#pitch')
+
+let voices = []
+
+const getVoices = () => {
+    voices = synth.getVoices();
+
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+        option.textContent = voice.name + '(' + voice.lang + ')';
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    });
+}
+
+getVoices();
+if(synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+}
